977f2eeadad8784b66f520c089bd8a185ee15353
asliddin2002developer <eshonkulovasliddindeveloper@gmail.com>
2023-01-07 19:00:09 +0500

    Maven Task Update

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,10 +1,12 @@
 package com.epam.demo;
 import com.epam.utils.StringUtils;
+
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
 
         return args.stream().allMatch(StringUtils::isPositiveNumber);
+
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,4 +1,5 @@
 package com.epam.utils;
+
 import org.apache.commons.lang3.math.NumberUtils;
 
 public class StringUtils {
